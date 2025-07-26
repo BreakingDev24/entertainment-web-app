344881d99a84a34727a38fd60e32dc1aeb87ba3a
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-26 17:35:06 +0200

    feat(components): add Navbar component for layout
    - Introduced a new Navbar component to enhance the layout of the application.
    - Integrated navigation links using CustomNavLink for routing.
    - Included logo and avatar image for branding and user identification.
    - Utilized utility function `cn` for conditional class names.

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,44 @@
+import cn from "../../utils/cn";
+import Logo from "../../assets/logo.svg?react";
+import IconHome from "../../assets/icon-nav-home.svg?react";
+import IconMovies from "../../assets/icon-nav-movies.svg?react";
+import IconTvSeries from "../../assets/icon-nav-tv-series.svg?react";
+import IconBookmark from "../../assets/icon-nav-bookmark.svg?react";
+import ImageAvatar from "../../assets/image-avatar.png";
+import CustomNavLink from "../common/CustomNavlink";
+
+interface NavProps {
+  className?: string;
+}
+export default function Navbar({ className }: NavProps) {
+  return (
+    <div
+      className={cn(
+        "bg-darkBlue flex items-center justify-between p-4",
+        className,
+      )}
+    >
+      <div>
+        <Logo />
+      </div>
+      <nav className="r flex gap-6">
+        <CustomNavLink to="/" className="text-white">
+          <IconHome />
+        </CustomNavLink>
+        <CustomNavLink to="/movies">
+          <IconMovies />
+        </CustomNavLink>
+        <CustomNavLink to="/tv">
+          <IconTvSeries />
+        </CustomNavLink>
+        <CustomNavLink to="/bookmark">
+          <IconBookmark />
+        </CustomNavLink>
+      </nav>
+
+      <div>
+        <img src={ImageAvatar} alt="avatar" className="w-6" />
+      </div>
+    </div>
+  );
+}

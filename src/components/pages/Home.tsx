c176801d04011344dc4701e2ff5bf034cdbd8bd6
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-28 12:18:52 +0200

    fix(ui): replaced isLoading with isFetching to show loading on query change
    Replaced the use of `isLoading` with `isFetching` to correctly display the loading
    state when the search query changes. `isLoading` only applies to the initial load,
    while `isFetching` also triggers on background refetches, ensuring better UX.

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,7 +7,7 @@ import Card, { type MediaItem } from "../ui/Card";
 export default function Home() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
-  const { data, isLoading } = useSearchByTypeQuery(
+  const { data, isFetching } = useSearchByTypeQuery(
     { type: "multi", query: query ?? "" },
     { skip: !query },
   );
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className="p-4 text-white">
       <p>Results for: {query?.toUpperCase()}</p>
-      {isLoading ? (
+      {isFetching ? (
         <p>Loading</p>
       ) : (
         <ul className="grid grid-cols-2 justify-center justify-items-center gap-4 md:grid-cols-3">

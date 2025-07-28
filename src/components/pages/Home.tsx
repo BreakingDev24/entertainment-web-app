de1b371e21029cc14ffffccd210f25d7684779d9
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-28 11:57:39 +0200

    feat(pages): add Home component for displaying search results
    - Implemented the Home component to handle search queries and display results.
    - Utilized `useSearchParams` for query parameter management.
    - Integrated `useSearchByTypeQuery` to fetch data based on the search query.
    - Added loading state handling and conditional rendering for search results.
    - Displayed results in a responsive grid layout using the Card component.

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,33 @@
+import { useSearchParams } from "react-router-dom";
+import { useSearchByTypeQuery } from "../../features/tmdb/tmdbApi";
+import IconTypeMovie from "../../assets/icon-category-movie.svg?react";
+import IconTypeTV from "../../assets/icon-category-tv.svg?react";
+import Card, { type MediaItem } from "../ui/Card";
+
+export default function Home() {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
+  const { data, isLoading } = useSearchByTypeQuery(
+    { type: "multi", query: query ?? "" },
+    { skip: !query },
+  );
+
+  const imgUrl = "https://image.tmdb.org/t/p/";
+
+  return (
+    <div className="p-4 text-white">
+      <p>Results for: {query?.toUpperCase()}</p>
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <ul className="grid grid-cols-2 justify-center justify-items-center gap-4 md:grid-cols-3">
+          {data?.map((item: MediaItem) => (
+            <li key={item.id} className="grid w-full justify-items-center">
+              <Card item={item} imgUrl={imgUrl} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}

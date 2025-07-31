ad5ed65700f47c0f5c0f654d63c99bdba312d0e9
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-31 21:49:13 +0200

    feat(search): create SearchForm component with dynamic routing and placeholder
    Introduced a new SearchForm component that handles input-based navigation
    depending on the current route. It adjusts both the destination path
    and the placeholder text dynamically based on whether the user is on
    /movies, /tv, or another route.

---
+++ src/components/layout/SearchForm.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import IconSearch from "../../assets/icon-search.svg?react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function SearchForm() {
+  const [input, setInput] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const path = location.pathname;
+    const params = new URLSearchParams({ query: input });
+    let basePath = "/results";
+    if (path.includes("/movies")) basePath = "/movies/results";
+    if (path.includes("/tv")) basePath = "/tv/results";
+
+    navigate(`${basePath}?${params.toString()}`);
+  };
+  const getPlaceholder = () => {
+    if (location.pathname === "/movies") return "Search for movies";
+    if (location.pathname === "/tv") return "Search for TV shows";
+    return "Search for movies or TV series";
+  };
+  return (
+    <form action="" onSubmit={handleSubmit} className="flex">
+      <button>
+        <IconSearch />
+      </button>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder={getPlaceholder()}
+        className="text-preset2-mobile flex-1 text-white placeholder-gray-300"
+      />
+    </form>
+  );
+}

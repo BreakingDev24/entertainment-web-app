342127f91a8ac0ac318dc0fcd43f7421e84499bb
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-01 00:10:16 +0200

    feat(store): add bookmark slice to Redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApi } from "../features/tmdb/tmdbApi";
-
+import bookmarkReducer from "../features/bookmark/bookmark";
 export const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    bookmark: bookmarkReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tmdbApi.middleware),

c9ebc45ea16a432e9dceba0d5563254645b75a37
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-01 00:12:04 +0200

    feat(card): add bookmark toggle functionality to media cards
    -Enhanced the Card component to connect with Redux bookmark state.
    -Added bookmark button that toggles movie or TV show bookmarks, with visual feedback showing bookmarked status.

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -2,6 +2,10 @@ import { useLocation } from "react-router-dom";
 import IconTypeMovie from "../../assets/icon-category-movie.svg?react";
 import IconTypeTV from "../../assets/icon-category-tv.svg?react";
 import cn from "../../utils/cn";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { addBookmark, removeBookmark } from "../../features/bookmark/bookmark";
+import BookmarkEmpty from "../../assets/icon-bookmark-empty.svg";
+import BookmarkFull from "../../assets/icon-bookmark-full.svg";
 
 export type MovieItem = {
   id: number;
@@ -28,6 +32,7 @@ interface CardProps {
 
 export default function Card({ item, className }: CardProps) {
   const location = useLocation();
+  const dispatch = useAppDispatch();
   const imgUrl = "https://image.tmdb.org/t/p/";
   const mediaType =
     item.media_type ||
@@ -47,11 +52,21 @@ export default function Card({ item, className }: CardProps) {
     "title" in item ? item.title : "name" in item ? item.name : "Unkonwn";
   const mediaTypeIcon =
     mediaType === "movie" ? <IconTypeMovie /> : <IconTypeTV />;
-
+  const bookmarks = useAppSelector((state) =>
+    item.media_type === "movie" ? state.bookmark.movie : state.bookmark.tv,
+  );
+  const isBookmarked = bookmarks.some((el) => el.id === item.id);
+  const handleToggleBookmark = () => {
+    if (isBookmarked)
+      dispatch(removeBookmark({ id: item.id, media_type: item.media_type }));
+    else dispatch(addBookmark(item));
+    console.log("Bookmarks attuali:", bookmarks);
+    console.log(isBookmarked);
+  };
   return (
     <div
       className={cn(
-        "grid w-full justify-items-center gap-1.5 text-white",
+        "relative grid w-full justify-items-center gap-1.5 text-white",
         className,
       )}
     >
@@ -66,9 +81,18 @@ export default function Card({ item, className }: CardProps) {
           {mediaTypeIcon}
           {mediaType}
         </span>
-        <span></span>
       </div>
       <p className="text-preset4-mobile">{mediaTitle}</p>
+      <button
+        onClick={handleToggleBookmark}
+        className="bg-darkBlue absolute top-2 right-2 rounded-full p-2 opacity-50"
+      >
+        <img
+          className="w-3"
+          src={isBookmarked ? BookmarkFull : BookmarkEmpty}
+          alt=""
+        />
+      </button>
     </div>
   );
 }

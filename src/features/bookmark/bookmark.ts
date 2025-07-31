e206949f73726f365139fd25982184acc6817745
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-01 00:08:46 +0200

    feat(bookmark): add initial bookmark slice with movie/tv separation
    -Added new Redux slice to manage bookmarks.
    -The state keeps `movie` and `tv` items in separate arrays.
    -Includes `addBookmark` and `removeBookmark` reducers

---
+++ src/features/bookmark/bookmark.ts
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../app/store";
+import { type MovieItem, type TVItem } from "../../components/ui/Card";
+
+interface BookmarkState {
+  movie: MovieItem[];
+  tv: TVItem[];
+}
+
+type BookmarkPayload = MovieItem | TVItem;
+
+const initialState: BookmarkState = {
+  movie: [],
+  tv: [],
+};
+
+const bookmarkSlice = createSlice({
+  name: "bookmark",
+  initialState,
+  reducers: {
+    addBookmark(state, action: PayloadAction<BookmarkPayload>) {
+      const item = action.payload;
+      if (item.media_type === "movie") {
+        if (!state.movie.some((el) => el.id === item.id)) {
+          state.movie.push(item);
+        }
+      }
+      if (item.media_type === "tv") {
+        if (!state.tv.some((el) => el.id === item.id)) {
+          state.tv.push(item);
+        }
+      }
+    },
+    removeBookmark(
+      state,
+      action: PayloadAction<{ id: number; media_type: "movie" | "tv" }>,
+    ) {
+      const { id, media_type } = action.payload;
+      if (media_type === "movie")
+        state.movie = state.movie.filter((item) => item.id !== id);
+      if (media_type === "tv")
+        state.tv = state.tv.filter((item) => item.id !== id);
+    },
+  },
+});
+
+export const { addBookmark, removeBookmark } = bookmarkSlice.actions;
+export default bookmarkSlice.reducer;

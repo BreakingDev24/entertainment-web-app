bdcf8f7e244a625745fa0589fcb7494fb4f6da67
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-04 01:13:44 +0200

    feat(routing): add React Router setup with main app pages and layout components
    This refactor sets the foundation for a multi-page React app with dynamic routing,
    enabling users to navigate between different sections without page reloads.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Navbar from "./components/layout/Navbar";
+import { useSearchByTypeQuery } from "./features/tmdb/tmdbApi";
+import { useState } from "react";
+import SearchForm from "./components/layout/SearchForm";
+import Home from "./components/pages/Home";
+import Movies from "./components/pages/Movies";
+import TVShows from "./components/pages/TVShows";
+import ResultPage from "./components/pages/ResultsPage";
+import BookmarksPage from "./components/pages/BookmarkPage";
+type SearchType = "movie" | "multi" | "tv";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="bg-darkBlue min-h-screen">
+          <Navbar />
+          <SearchForm />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tv" element={<TVShows />} />
+            <Route path="/bookmark" element={<BookmarksPage />} />
+            <Route path="/:section?/results" element={<ResultPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;

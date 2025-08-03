2f55f033d2d80412fc6e4686629ed79ccb534390
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-03 18:48:07 +0200

    refactor(home): remove search query logic now handled in ResultsPage
    Moved the search-related query and UI logic out of the Home page, since search results are now displayed in the dedicated ResultsPage component.

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,5 @@
 import { useSearchParams } from "react-router-dom";
-import {
-  useGetTrendingQuery,
-  useSearchByTypeQuery,
-} from "../../features/tmdb/tmdbApi";
+import { useGetTrendingQuery } from "../../features/tmdb/tmdbApi";
 import List from "../common/List";
 
 export default function Home() {
@@ -11,20 +8,8 @@ export default function Home() {
   const { data: trendingData, isFetching: isFetchingTrending } =
     useGetTrendingQuery();
 
-  const { data: queryData, isFetching: isFetchingQuery } = useSearchByTypeQuery(
-    { type: "multi", query: query ?? "" },
-    { skip: !query },
-  );
-
   return (
     <div className="p-4 text-white">
-      {query && (
-        <div>
-          <h2>Results for: {query?.toUpperCase()}</h2>
-          {isFetchingQuery ? <p>Loading</p> : <List data={queryData} />}
-        </div>
-      )}
-
       {!query && (
         <div>
           <h2>Trending</h2>

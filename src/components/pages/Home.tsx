1832f8e04f9f3fcc59f892d617fd29f5634a54eb
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-04 10:39:59 +0200

    feat(home): add now playing movies section with horizontal scrolling list
    Fetched now playing movies using useGetNowPlayingQuery and displayed them in a horizontally scrollable List component using the 'secondary' variant.

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,8 @@
 import { useSearchParams } from "react-router-dom";
-import { useGetTrendingQuery } from "../../features/tmdb/tmdbApi";
+import {
+  useGetNowPlayingQuery,
+  useGetTrendingQuery,
+} from "../../features/tmdb/tmdbApi";
 import List from "../common/List";
 
 export default function Home() {
@@ -7,19 +10,30 @@ export default function Home() {
   const query = searchParams.get("query");
   const { data: trendingData, isFetching: isFetchingTrending } =
     useGetTrendingQuery();
-
+  const { data: nowPlayingData, isFetching: isFetchingNowPlaying } =
+    useGetNowPlayingQuery();
+  console.log(nowPlayingData);
   return (
-    <div className="p-4 text-white">
-      {!query && (
-        <div>
-          <h2>Trending</h2>
-          {isFetchingTrending ? (
+    <div className="overflow-hidden p-4 text-white">
+      <div>
+        <h2>Now Playing Movies</h2>
+        <div className="overflow-x-auto">
+          {isFetchingNowPlaying ? (
             <p>Loading</p>
           ) : (
-            <List data={trendingData.results} />
+            <List data={nowPlayingData} variant="secondary" />
           )}
         </div>
-      )}
+      </div>
+
+      <div>
+        <h2>Trending</h2>
+        {isFetchingTrending ? (
+          <p>Loading</p>
+        ) : (
+          <List data={trendingData.results} />
+        )}
+      </div>
     </div>
   );
 }

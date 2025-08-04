5938d2da2680ff55521c9e6382c43ca73488909b
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-04 10:28:45 +0200

    feat(list): add support for horizontal scroll layout with secondary variant
    Introduced a 'secondary' variant for the List component to support horizontal scrolling layouts using grid-flow-col and auto-cols.

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -1,15 +1,30 @@
+import cn from "../../utils/cn";
 import Card, { type MediaItem } from "../ui/Card";
+
+type Variant = "primary" | "secondary";
 
 interface ListProps {
   data: MediaItem[];
+  variant?: Variant;
+  className?: string;
 }
 
-export default function List({ data }: ListProps) {
+export default function List({
+  data,
+  variant = "primary",
+  className,
+}: ListProps) {
+  const variants: Record<Variant, string> = {
+    primary:
+      "grid grid-cols-2 justify-center justify-items-center gap-4 md:grid-cols-3",
+    secondary: " grid grid-flow-col auto-cols-[240px] gap-5",
+  };
+
   return (
-    <ul className="grid grid-cols-2 justify-center justify-items-center gap-4 md:grid-cols-3">
+    <ul className={cn(variants[variant], className)}>
       {data?.map((item) => (
         <li key={item.id} className="grid w-full justify-items-center">
-          <Card item={item} />
+          <Card item={item} variant={variant} />
         </li>
       ))}
     </ul>

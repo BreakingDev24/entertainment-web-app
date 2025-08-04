8536684f89494b56caf69edbe96bf4f04432d5a8
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-04 10:38:33 +0200

    feat(bookmarks): create BookmarksPage to display saved movies and TV shows
    Added a new component to render bookmarked movies and TV shows using the global Redux state. Utilizes the List component for consistent layout.

---
+++ src/components/pages/BookmarkPage.tsx
@@ -0,0 +1,18 @@
+import { useAppSelector } from "../../app/hooks";
+import List from "../common/List";
+
+export default function BookmarksPage() {
+  const { movie, tv } = useAppSelector((state) => state.bookmark);
+  return (
+    <div className="text-white">
+      <section>
+        <h2>Movie Bookmarks</h2>
+        <List data={movie} />
+      </section>
+      <section>
+        <h2>TV shows Bookmarks</h2>
+        <List data={tv} />
+      </section>
+    </div>
+  );
+}

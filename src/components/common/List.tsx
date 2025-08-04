61676a63ff598bb80443a566391c1322904f9311
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-04 22:25:28 +0200

    refactor(list): use tailwind-variants and update  import path

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -1,5 +1,7 @@
-import cn from "../../utils/cn";
-import Card, { type MediaItem } from "../ui/Card";
+import cn from "@/utils/cn";
+import Card from "@/components/ui/Card";
+import type { MediaItem } from "@/components/ui/Card/types";
+import { tv } from "tailwind-variants";
 
 type Variant = "primary" | "secondary";
 
@@ -9,21 +11,34 @@ interface ListProps {
   className?: string;
 }
 
+const listStyle = tv({
+  slots: {
+    root: "",
+    listElement: "grid w-full justify-items-center",
+  },
+  variants: {
+    variant: {
+      primary: {
+        root: "grid grid-cols-2 justify-center justify-items-center gap-4 md:grid-cols-3",
+      },
+      secondary: {
+        root: "grid auto-cols-[240px] grid-flow-col gap-5",
+      },
+    },
+  },
+});
+
 export default function List({
   data,
   variant = "primary",
   className,
 }: ListProps) {
-  const variants: Record<Variant, string> = {
-    primary:
-      "grid grid-cols-2 justify-center justify-items-center gap-4 md:grid-cols-3",
-    secondary: " grid grid-flow-col auto-cols-[240px] gap-5",
-  };
+  const styles = listStyle({ variant });
 
   return (
-    <ul className={cn(variants[variant], className)}>
+    <ul className={cn(styles.root(), className)}>
       {data?.map((item) => (
-        <li key={item.id} className="grid w-full justify-items-center">
+        <li key={item.id} className={styles.listElement()}>
           <Card item={item} variant={variant} />
         </li>
       ))}

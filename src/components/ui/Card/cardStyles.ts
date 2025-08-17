566dbcf614f0e560154ba1ae891608e4d3ccc1d3
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-08-17 16:12:44 +0200

    style(Card): update title styles for better text handling
    - Enhanced title styles to ensure proper text overflow handling.
    - Added classes for full width, text ellipsis, and whitespace management.

--- src/components/ui/Card/cardStyles.ts
+++ src/components/ui/Card/cardStyles.ts
@@ -7,7 +7,8 @@ export const cardVariants = tv({
     content: "grid w-full gap-1.5 text-center",
     meta: "text-preset6-mobile flex w-full",
     typeLabel: "flex items-center gap-1.5",
-    title: "text-preset4-mobile",
+    title:
+      "text-preset4-mobile w-full overflow-hidden text-ellipsis whitespace-nowrap",
     bookmark: "bg-darkBlue absolute top-2 right-2 rounded-full p-2 opacity-50",
   },
   variants: {
